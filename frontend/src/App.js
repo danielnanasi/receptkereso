a228e382d78bdc0fcb97cadd860ea630891c652a
ricsik52 <ricsik52@gmail.com>
2020-11-02 15:57:37 +0100

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchField from "react-search-field";
+import SearchAppBar from './search';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <header>  
+    
+    <SearchAppBar />
+
+       
+    </header>   
+    </div>
+  );
+}
+
+export default App;
