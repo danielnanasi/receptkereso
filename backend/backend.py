f9cb27cf0a90b49855c279dc806e1c29334bd1a7
Nánási Dániel <nanasidnl@gmail.com>
2020-11-18 19:21:44 +0100

    Extend with meal types

--- backend/backend.py
+++ backend/backend.py
@@ -10,10 +10,17 @@ spooncalcular_api_key="apiKey=55c3167293d4431289eb466abf605bb4"
 def hello_world():
     return "Awsome receptkereső API \n developed by CSAPAT-6 \n Internet szolgáltatások és alkalmazások házi feladataként \n 2020 ősz"
 
-@app.route('/search/<query_string>')
-def spooncalcular_search(query_string, vega=False, glutenmentes=False, cukormentes=False):
+@app.route('/search/<query_string>/<options>')
+def spooncalcular_search(query_string, options="000000"):
     # spooncalular api doksi: https://spoonacular.com/food-api/docs#Search-Recipes-Complex
+    vega=options[0]
+    glutenmentes=options[1]
+    cukormentes=options[2]
 
+    leves=options[3]
+    foetel=options[4]
+    desszert=options[5]
+    
     query = "&query=" + str(query_string.replace(' ', ','))
     special=""
     if vega:
@@ -22,10 +29,18 @@ def spooncalcular_search(query_string, vega=False, glutenmentes=False, cukorment
         special="&intolerance=gluten"
     if cukormentes:
         special="&intolerance=sugar"
-    response = requests.get("https://api.spoonacular.com/recipes/complexSearch?"+spooncalcular_api_key + query + special )
+    
+    meal_type=""
+    if leves:
+        meal_type="&type=soup"
+    if foetel:
+        meal_type="&type=main%20course"
+    if desszert:
+        meal_type="&type=dessert"
 
-    return response.text
+    spooncacular_response = requests.get("https://api.spoonacular.com/recipes/complexSearch?"+spooncalcular_api_key + query + special + meal_type )
 
+    return spooncacular_response.text 
 
 @app.route('/detail/<object_id>')
 def spooncalcular_detail(object_id):
